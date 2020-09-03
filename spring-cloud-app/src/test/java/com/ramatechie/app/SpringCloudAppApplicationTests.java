fffd4304299e76d5fa093e8dc796d7b44fcf8f4b
ramaki5 <rk.mundru@gmail.com>
2020-09-04 01:37:46 +0530

    spring cloud app

---
+++ spring-cloud-app/src/test/java/com/ramatechie/app/SpringCloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramatechie.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
